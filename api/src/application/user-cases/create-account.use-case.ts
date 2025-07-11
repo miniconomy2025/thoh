b31567d40972bb1cd254d69a69c8dd9fb765c99e
rdngobeni <rivo.ngobeni@standardbank.co.za>
2025-07-11 05:30:16 +0200

    create account request body

--- api/src/application/user-cases/create-account.use-case.ts
+++ api/src/application/user-cases/create-account.use-case.ts
@@ -1,4 +1,8 @@
 import { Person } from "../../domain/population/person.entity";
+import {Agent} from "undici";
+import fs from "fs";
+import path from "node:path";
+import { fetch } from 'undici';
 
 export class CreateAccountUseCase {
   private person: Person;
@@ -9,15 +13,26 @@ export class CreateAccountUseCase {
   async execute(): Promise<void> {
     if (!this.person.accountNumber) {
 
-      const body = { salaryCents: Math.floor(this.person.salary * 100)};
+      const agent = new Agent({
+        connect: {
+          cert : fs.readFileSync(path.join(__dirname, 'thoh-client.crt')),
+          key : fs.readFileSync(path.join(__dirname, 'thoh-client.key')),
+          rejectUnauthorized: false
+        }
+      });
+
+      const body = { salary: Math.floor(this.person.salary * 100)};
       const createAccountResponse = await fetch(process.env.RETAIL_BANK_API_URL + '/accounts', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify(body)
+        body: JSON.stringify(body),
+        dispatcher: agent,
       });
-      const account = await createAccountResponse.json();
+
+      const account = await createAccountResponse.body;
+      // @ts-ignore
       this.person.accountNumber = account.accountId;
     } else {
       // person already has an account, no need to create another one
