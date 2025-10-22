2365110c82425ef8adfc66b7be1be52f3e1a9770
rdngobeni <rivo.ngobeni@standardbank.co.za>
2025-10-22 21:53:33 +0200

    .

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const CONSTANTS = {
-  API_URL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+  API_URL: "https://ec2-13-244-65-62.af-south-1.compute.amazonaws.com",
   SIMULATION_SYNC_INTERVAL: 2 * 60 * 1000, // 2 minutes in ms ,
   SIMULATION_SECOND_IN_MS: 12 * 60 * 1000,
   COMMERCIAL_BANK_URL: import.meta.env.VITE_COMMERCIAL_BANK_URL || 'http://localhost:3000',
